74fc0fb1cabdc61745cf2d03519d61b3175095ac
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2025-11-14 08:36:27 -0300

    corrigindo a aparência do icone de cancel

--- src/modules/schedules/show.js
+++ src/modules/schedules/show.js
@@ -29,7 +29,7 @@ export function schedulesShow({ dailySchedules }) {
             // Cria o ícone de cancelar o agendamento.
             const cancelIcon = document.createElement('img')
             cancelIcon.classList.add('cancel-icon')
-            cancelIcon.setAttribute("src", "../../src/assets/cancel.svg")
+            cancelIcon.setAttribute("src", "./src/assets/cancel.svg")
             cancelIcon.setAttribute("alt", "Cancelar")
             
             // Adiciona o tempo, nome e icone no item (li).
